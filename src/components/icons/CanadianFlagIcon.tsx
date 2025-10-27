e698a47f085465865b2d711a2d78cfe179f6e667
Abdul Basit <bkbasit73@gmail.com>
2025-10-27 01:46:11 +0000

    file this area with canadina flag

--- src/components/icons/CanadianFlagIcon.tsx
+++ src/components/icons/CanadianFlagIcon.tsx
@@ -4,15 +4,15 @@ export function CanadianFlagIcon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 80 40"
+      viewBox="0 0 1200 600"
       {...props}
     >
-      <rect width="80" height="40" fill="#fff" />
-      <rect width="20" height="40" fill="#d52b1e" />
-      <rect x="60" width="20" height="40" fill="#d52b1e" />
+      <rect width="1200" height="600" fill="#fff" />
+      <rect width="300" height="600" fill="#d52b1e" />
+      <rect x="900" width="300" height="600" fill="#d52b1e" />
       <path
-        d="M40 11.5l-4.5 3-1.5 6h-6l4.5 4-1.5 6 7.5-4.5 7.5 4.5-1.5-6 4.5-4h-6l-1.5-6z"
         fill="#d52b1e"
+        d="M600 150l-50 50h-100l25 50-75 100 100-50 50 150 50-150 100 50-75-100 25-50h-100z"
       />
     </svg>
   );
