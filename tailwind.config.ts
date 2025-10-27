cf58b3eab05aee77acd901f4058ff25d50a968b9
Abdul Basit <bkbasit73@gmail.com>
2025-10-27 05:21:46 +0000

    how much cad dollar i will pay for publish

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['"PT Sans"', 'sans-serif'],
-        headline: ['"PT Sans"', 'sans-serif'],
+        body: ['"EB Garamond"', 'serif'],
+        headline: ['"EB Garamond"', 'serif'],
         code: ['monospace'],
       },
       colors: {
